5f4135fd7512e8ccf89d91164f51803c8b5727a8
Vazha Khayri <khayrivazhasocial@gmail.com>
2025-01-31 14:18:24 +0700

    feat: second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-blue-500">Hello, Hackvidia!!!</h1>
+      <h1 className="text-3xl text-blue-500">Hello, Hackvidia!!</h1>
     </>
   );
 }
